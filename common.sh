03bd094d1a58900ec7b3cad3e2fd06f7e7c44b8c
sravya <ksravya66@gmail.com>
2026-07-05 16:05:38 +0530

    roboshop-common

--- common.sh
+++ common.sh
@@ -8,8 +8,11 @@ N="\e[0m"
 
 LOGS_FOLDER="/var/log/shell-roboshop"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+SCRIPT_DIR=$PWD
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
 START_TIME=$(date +%s)
+MONGODB_HOST=mongodb.ayaansh123.fun
+
 
 mkdir -p $LOGS_FOLDER
 echo "Script started executed at: $(date)" | tee -a $LOG_FILE
@@ -30,6 +33,50 @@ VALIDATE(){ # functions receive inputs through args just like shell script args
     fi
 }
 
+nodejs_setup(){
+    dnf module disable nodejs -y &>>LOG_FILE
+    VALIDATE $? "Disabling NodeJS"
+    dnf module enable nodejs:20 -y &>>LOG_FILE
+    VALIDATE $? "Enabling NodeJS"
+    dnf install nodejs -y &>>LOG_FILE
+    VALIDATE $? "Installing NodeJS"
+
+    npm install &>>LOG_FILE
+    VALIDATE $? "Installing dependencies"
+}
+
+app_setup(){
+    id roboshop &>>$LOG_FILE
+    if [ $? -ne 0 ]; then
+        useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
+        VALIDATE $? "Creating system user"
+    else
+        echo -e "User already exist ... $Y SKIPPING $N"
+    fi
+    mkdir -p /app 
+    VALIDATE $? "Creating App Directory"
+    curl -o /tmp/$app_name.zip https://roboshop-artifacts.s3.amazonaws.com/$app_name-v3.zip &>>LOG_FILE
+    VALIDATE $? "Downloading $app_name application"
+    cd /app 
+    VALIDATE $? "Changing to app directory"
+    rm -rf /app/*
+    VALIDATE $? "Removing existing code"
+    unzip /tmp/$app_name.zip &>>LOG_FILE
+    VALIDATE $? "Unzip $app_name"
+}
+
+systemd_setup(){
+    cp $SCRIPT_DIR/$app_name.service /etc/systemd/system/$app_name.service 
+    VALIDATE $? "copy systemctl service"
+    systemctl daemon-reload
+    systemctl enable $app_name &>>LOG_FILE
+    VALIDATE $? "Enable $app_name"
+}
+
+app_restart(){
+    systemctl restart $app_name
+    $VALIDATE $? "Restarted $app_name"
+}
 print_total_time(){
 END_TIME=$(date +%s)
 TOTAL_TIME=$(($END_TIME - $START_TIME))
