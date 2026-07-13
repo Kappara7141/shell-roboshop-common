64987ff761e8773a157d5f849a83c94746ec6247
sravya <ksravya66@gmail.com>
2026-07-13 16:51:41 +0530

    roboshop-common

--- mysql.sh
+++ mysql.sh
@@ -3,11 +3,12 @@
 source ./common.sh
 
 check_root
-dnf install mysql-server -y &>>LOG_FILE
-VALIDATE $? "Installing MYSQL Server"
-systemctl enable mysqld &>>LOG_FILE
+
+dnf install mysql-server -y &>>$LOG_FILE
+VALIDATE $? "Installing MySQL Server"
+systemctl enable mysqld &>>$LOG_FILE
 systemctl start mysqld  
-VALIDATE $? "Enabling and Starting MYSQL Server"
-mysql_secure_installation --set-root-pass RoboShop@1 &>>LOG_FILE
-VALIDATE $? "Setting root password to MYSQL Server"
+VALIDATE $? "Enabling and Starting MySQL Server"
+mysql_secure_installation --set-root-pass RoboShop@1 &>>$LOG_FILE
+VALIDATE $? "Setting root password to MySQL Server"
 print_total_time
