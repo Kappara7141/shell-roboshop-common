d3c53781c267be317b4db71c84f4ffe19b2055b4
sravya <ksravya66@gmail.com>
2026-07-13 16:15:33 +0530

    roboshop-common

--- payment.sh
+++ payment.sh
@@ -1,12 +1,10 @@
-#!/bin/bash
-
 source ./common.sh
-app_name=shipping
+app_name=payment
 
 check_root
 app_setup
-nodejs_setup
-systemd_setup
 python_setup
+systemd_setup
+
 app_restart
 print_total_time
